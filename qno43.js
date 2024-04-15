806a7b4696219b131bb655908e7773dee80522a9
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-04-16 01:31:18 +0500

    q no 43 of 45 Assignment

---
+++ qno43.js
@@ -0,0 +1,18 @@
+"use strict";
+// Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s names.
+Object.defineProperty(exports, "__esModule", { value: true });
+const magicians = ['David Copperfield', 'Criss Angel', 'Harry Houdini', 'Teller'];
+function showMagicians(magicians) {
+    console.log(magicians.join("\n"));
+}
+function makeGreat(magicians) {
+    return magicians.map((magician) => `${magician} the Great`);
+}
+const magiciansCopy = [...magicians];
+const greatMagicians = makeGreat(magiciansCopy);
+//ORIGINAL MAGICIANS:
+console.log("Original magicians:");
+showMagicians(magicians);
+//WITH GREAT:
+console.log("\nMagicians with 'the Great' added:");
+showMagicians(greatMagicians);
---
+++ qno43.ts
@@ -0,0 +1,20 @@
+// Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s names.
+
+type magician = string
+const magicians: magician[] = ['David Copperfield', 'Criss Angel', 'Harry Houdini', 'Teller'];
+
+function showMagicians(magicians: magician[]): void {
+  console.log(magicians.join("\n"));
+}
+function makeGreat(magicians: magician[]): magician[] {
+  return magicians.map((magician) => `${magician} the Great`);
+}
+const magiciansCopy = [...magicians];
+const greatMagicians = makeGreat(magiciansCopy);
+//ORIGINAL MAGICIANS:
+console.log("Original magicians:");
+showMagicians(magicians);
+//WITH GREAT:
+console.log("\nMagicians with 'the Great' added:");
+showMagicians(greatMagicians);
+
